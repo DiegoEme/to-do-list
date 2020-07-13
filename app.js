6a4bd03c19afa9652e9568ddc02720b145bdd361
Diego <soterg.uhdeia@gmail.com>
2020-07-12 21:29:14 -0500

    check agregado

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var tareaPorHacer = document.querySelector(".asignar-tarea");
 var elementos = document.getElementById("lista").children;
-var elementos = document.getElementsByTagName("div");
+//var elementos = document.getElementsByTagName("div");
 
-function borrar() {
+function borrar(e) {
   elementos[elementos.length - 1].remove();
+  if (elementos.length == 0) {
+    alert("No hay más tareas");
+  }
 }
 
 function agregar() {
@@ -11,4 +14,19 @@ function agregar() {
   e.innerText = tareaPorHacer.value;
   e.className = "item";
   document.getElementById("lista").appendChild(e);
+  tareaPorHacer.value = "";
+
+  var check = document.createElement("button");
+  check.innerHTML = '<i class="fas fa-check"></i>';
+  check.className = "check";
+  e.appendChild(check);
+}
+
+function myFunction(event) {
+  let item = event.target;
+  console.log(item);
+  if (item.className === "check" || item.className === "fas fa-check") {
+    var completado = item.parentElement;
+    completado.classList.toggle("completed");
+  }
 }
